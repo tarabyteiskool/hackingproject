06638924f33ae0bb9fbbd40629c19d56db381e10
Tara Byte <tarabyteiskool@proton.me>
2023-11-07 22:14:43 +1100

    first commit lol

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+
+const app = express();
+const port = 3000;
+
+app.use(basicAuth({
+    users: { 'admin': 'flag{ItsHereForeverNow}' },
+    challenge: true,
+    realm: 'My Application',
+}));
+
+app.get('/', (req, res) => {
+    res.send('You are authenticated!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
